f648c1dc12658846da8c9e206031e8c63ebf2305
Bryan Hughes <khan@nmt.edu>
2010-03-24 16:02:58 -0600

    Working on dispatching

--- forge/__init__.py
+++ forge/__init__.py
@@ -1,9 +1,10 @@
 
 import os,sys
+import os.path
+import json
 import ConfigParser
 import sqlalchemy
-import os.path
-
+import traceback
 
 #Get forge configuration
 config = ConfigParser.ConfigParser()
@@ -19,3 +20,28 @@ import forge.commands.packages
 import forge.commands.config
 import forge.commands.password
 import forge.commands.kerberos
+
+
+
+class CommandNotFound(Exception):
+	def __init__(self,command):
+		self.command = command
+	def __str__(self):
+		return repr(self.command)
+	def message(self):
+		return "Command %s is invalid" % self.command
+	def backtrace(self):
+		exc_type, exc_value, exc_traceback = sys.exc_info()
+		tb =  " ".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
+		return " ".join(tb.split("\n"))
+class Commands:
+	def find(self,command,subcommand):
+		if not command:
+			raise CommandNotFound(command)
+		if command not in dir(forge.commands):
+			raise CommandNotFound(command)
+
+
+def invoke(command,subcommand,json):
+	Commands().find(command,subcommand)
+
